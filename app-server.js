b194e915b7461b874982d3db593fd032d1b4541d
Joshua <Joshua.Nezianya@gmail.com>
2017-01-11 17:18:27 -0600

    added notifications for sockets

--- app-server.js
+++ app-server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+var connections = [];
+
 app.use(express.static('./public'));
 app.use(express.static('./node_modules/bootstrap/dist'));
 
@@ -8,7 +10,15 @@ var server = app.listen(3000);
 var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function (socket) {
-    console.log("Connected: %s", socket.id);
+
+    socket.once('disconnect', function(){
+        connections.splice(connections.indexOf(socket), 1);
+        socket.disconnect();
+        console.log("Disconnected: %s sockets remaining.", connections.length);
+    });
+
+    connections.push(socket);
+    console.log("Connected: %s sockets connected", connections.length);
 });
 
 console.log("Polling server is running at 'http://localhost:3000'");
