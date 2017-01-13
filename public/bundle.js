c57e950a6502bd40290e245f6455c6eaf3857cc5
Joshua <Joshua.Nezianya@gmail.com>
2017-01-12 23:25:52 -0600

    adding questions for audience

--- public/bundle.js
+++ public/bundle.js
@@ -23579,7 +23579,7 @@
 	        return {
 	            status: 'disconnected',
 	            title: '',
-	            dance: 'yes please'
+	            member: {}
 	        };
 	    },
 
@@ -23588,6 +23588,7 @@
 	        this.socket.on('connect', this.connect);
 	        this.socket.on('disconnect', this.disconnect);
 	        this.socket.on('welcome', this.welcome);
+	        this.socket.on('joined', this.joined);
 	    },
 
 	    emit: function emit(eventName, payload) {
@@ -23604,6 +23605,10 @@
 
 	    welcome: function welcome(serverState) {
 	        this.setState({ title: serverState.title });
+	    },
+
+	    joined: function joined(member) {
+	        this.setState({ member: member });
 	    },
 
 	    render: function render() {
@@ -30898,11 +30903,30 @@
 					Display,
 					{ 'if': this.props.status === 'connected' },
 					React.createElement(
-						'h1',
-						null,
-						'Join the session'
+						Display,
+						{ 'if': this.props.member.name },
+						React.createElement(
+							'h2',
+							null,
+							'Welcome ',
+							this.props.member.name
+						),
+						React.createElement(
+							'p',
+							null,
+							'Questions will appear here.'
+						)
 					),
-					React.createElement(Join, { emit: this.props.emit })
+					React.createElement(
+						Display,
+						{ 'if': !this.props.member.name },
+						React.createElement(
+							'h1',
+							null,
+							'Join the session'
+						),
+						React.createElement(Join, { emit: this.props.emit })
+					)
 				)
 			);
 		}
