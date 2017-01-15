6e63b12d8ee05acd1f5ae8720c9afcf9046ecde3
Joshua <Joshua.Nezianya@gmail.com>
2017-01-14 22:59:44 -0600

    able to display and answer questions

--- public/bundle.js
+++ public/bundle.js
@@ -23645,6 +23645,7 @@
 	    },
 
 	    ask: function ask(question) {
+	        sessionStorage.answer = '';
 	        this.setState({ currentQuestion: question });
 	    },
 
@@ -30972,7 +30973,7 @@
 						React.createElement(
 							Display,
 							{ 'if': this.props.currentQuestion },
-							React.createElement(Ask, { question: this.props.currentQuestion })
+							React.createElement(Ask, { question: this.props.currentQuestion, emit: this.props.emit })
 						)
 					),
 					React.createElement(
@@ -31068,13 +31069,15 @@
 	'use strict';
 
 	var React = __webpack_require__(1);
+	var Display = __webpack_require__(251);
 
 	var Ask = React.createClass({
 		displayName: 'Ask',
 
 		getInitialState: function getInitialState() {
 			return {
-				choices: []
+				choices: [],
+				answer: undefined
 			};
 		},
 
@@ -31089,7 +31092,19 @@
 		setUpChoices: function setUpChoices() {
 			var choices = Object.keys(this.props.question);
 			choices.shift();
-			this.setState({ choices: choices });
+			this.setState({
+				choices: choices,
+				answer: sessionStorage.answer
+			});
+		},
+
+		select: function select(choice) {
+			this.setState({ answer: choice });
+			sessionStorage.answer = choice;
+			this.props.emit('answer', {
+				question: this.props.question,
+				choice: choice
+			});
 		},
 
 		addChoiceButton: function addChoiceButton(choice, i) {
@@ -31098,7 +31113,9 @@
 
 			return React.createElement(
 				'button',
-				{ key: i, className: "col-xs-12 col-sm-6 btn btn-" + buttonTypes[i] },
+				{ key: i,
+					className: "col-xs-12 col-sm-6 btn btn-" + buttonTypes[i],
+					onClick: this.select.bind(null, choice) },
 				choice,
 				': ',
 				this.props.question[choice]
@@ -31110,14 +31127,33 @@
 				'div',
 				{ id: "currentQuestion" },
 				React.createElement(
-					'h2',
-					null,
-					this.props.question.q
+					Display,
+					{ 'if': this.state.answer },
+					React.createElement(
+						'h3',
+						null,
+						'You answered: ',
+						this.state.answer
+					),
+					React.createElement(
+						'p',
+						null,
+						this.props.question[this.state.answer]
+					)
 				),
 				React.createElement(
-					'div',
-					{ className: "row" },
-					this.state.choices.map(this.addChoiceButton)
+					Display,
+					{ 'if': !this.state.answer },
+					React.createElement(
+						'h2',
+						null,
+						this.props.question.q
+					),
+					React.createElement(
+						'div',
+						{ className: "row" },
+						this.state.choices.map(this.addChoiceButton)
+					)
 				)
 			);
 		}
